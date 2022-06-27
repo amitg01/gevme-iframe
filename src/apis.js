7c3caf15944fbcdba2407f778a83aa86f0feb714
amitg01 <amitg.ghy@gmail.com>
2022-06-27 12:51:03 +0530

    updated main app url

--- src/apis.js
+++ src/apis.js
@@ -1,2 +1,2 @@
-export const MAIN_APP_URL = "https://main--dazzling-mochi-eaacfc.netlify.app/";
+export const MAIN_APP_URL = "https://dazzling-mochi-eaacfc.netlify.app/";
 export const USERS_URL = "https://jsonplaceholder.typicode.com/users";
